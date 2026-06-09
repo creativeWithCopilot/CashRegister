1a453c35111993b76d72170929cbfb5a5ceae5a2
creativeWithCopilot <wintheinhtun@hotmail.com>
2026-06-09 23:13:52 +0630

    feat: Register teh DbContext with DI

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using CashRegister.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure EF Core with sqlite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("EFCoreDBConnection")));
 
 var app = builder.Build();
 
